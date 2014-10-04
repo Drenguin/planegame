18257e8bcb0be03457d86d17b9499271c6526514
Patrick M <drenguino@gmail.com>
2014-10-04 14:53:04 -0700

    Accelerometer stuff

--- FighterPlanes/Classes/GameScene.m
+++ FighterPlanes/Classes/GameScene.m
@@ -7,39 +7,57 @@
 //
 
 #import "GameScene.h"
+#import <CoreMotion/CoreMotion.h>
 
 
-@implementation GameScene
+@implementation GameScene {
+    CMMotionManager *_motionManager;
+    CCSprite *_planeSprite;
+}
 
 
-+ (GameScene *)scene
-{
++ (GameScene *)scene {
     return [[self alloc] init];
 }
 
 // -----------------------------------------------------------------------
 
-- (id)init
-{
+- (id)init {
     // Apple recommend assigning self with supers return value
     self = [super init];
     if (!self) return(nil);
+    
+    _motionManager = [[CMMotionManager alloc] init];
     
     CGSize screenSize = [[CCDirector sharedDirector] viewSize];
     
     // Create a colored background (Dark Grey)
     CCSprite *background = [CCSprite spriteWithImageNamed:@"California.png"];
     
-    CCSprite *planeSprite = [CCSprite spriteWithImageNamed:@"plane.png"];
-    planeSprite.position = ccp(screenSize.width/2.0f, screenSize.height/2.0f);
-    planeSprite.scale = 2.0f;
-    [planeSprite.texture setAntialiased:NO];
+    _planeSprite = [CCSprite spriteWithImageNamed:@"plane.png"];
+    _planeSprite.position = ccp(screenSize.width/2.0f, screenSize.height/2.0f);
+    _planeSprite.scale = 2.0f;
+    [_planeSprite.texture setAntialiased:NO];
     
     [self addChild:background];
-    [self addChild:planeSprite];
+    [self addChild:_planeSprite];
     
     // done
     return self;
+}
+
+- (void)update:(CCTime)delta {
+    CMAccelerometerData *accelerometerData = _motionManager.accelerometerData;
+    CMAcceleration acceleration = accelerometerData.acceleration;
+}
+
+- (void)onEnter {
+    [super onEnter];
+    [_motionManager startAccelerometerUpdates];
+}
+
+- (void)onExit {
+    [_motionManager stopAccelerometerUpdates];
 }
 
 
