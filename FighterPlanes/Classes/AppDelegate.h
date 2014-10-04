19a7525626b250ac8099f98397739b22ede3f6bf
Patrick M <drenguino@gmail.com>
2014-10-04 13:20:48 -0700

    Initial Commit

---
+++ FighterPlanes/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  FighterPlanes
+//
+//  Created by Patrick Mc Gartoll on 10/4/14.
+//  Copyright Drenguin 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ FighterPlanes/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FighterPlanes
+//
+//  Created by Patrick Mc Gartoll on 10/4/14.
+//  Copyright Drenguin 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
