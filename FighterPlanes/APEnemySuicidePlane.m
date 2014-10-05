965f15c32c33b5e7353ae63aa081a900b0791551
areardon11 <areardon11@hotmail.com>
2014-10-04 19:58:12 -0700

    obstacle plane

--- FighterPlanes/APEnemySuicidePlane.m
+++ FighterPlanes/APEnemySuicidePlane.m
@@ -18,9 +18,13 @@
     
     self.scale = .5f;
     self.health = 1;
-    _speed = 1.5f;
+    _speed = 2.0f;
     
     return self;
+}
+
+- (void)update:(CCTime)delta {
+    self.position = ccp(self.position.x+delta*_speed*sin(CC_DEGREES_TO_RADIANS(self.rotation)), self.position.y+delta*_speed*cos(CC_DEGREES_TO_RADIANS(self.rotation)));
 }
 
 @end
--- FighterPlanes/APObstaclePlane.m
+++ FighterPlanes/APObstaclePlane.m
@@ -11,4 +11,20 @@
 
 @implementation APObstaclePlane
 
+- (id)init {
+    // Apple recommend assigning self with supers return value
+    self = [super initWithImageNamed:@"obstaclePlane.png"];
+    if (!self) return(nil);
+    
+    self.scale = .25f;
+    self.health = 1;
+    _speed = 1.0f;
+    
+    return self;
+}
+
+- (void)update:(CCTime)delta {
+    self.position = ccp(self.position.x+delta*_speed*sin(CC_DEGREES_TO_RADIANS(self.rotation)), self.position.y+delta*_speed*cos(CC_DEGREES_TO_RADIANS(self.rotation)));
+}
+
 @end
