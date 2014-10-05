c6037a78a1647e06908aa0a5100741c380456b19
areardon11 <areardon11@hotmail.com>
2014-10-04 19:29:32 -0700

    Added delta and obstacle plane

---
+++ FighterPlanes/APObstaclePlane.h
@@ -0,0 +1,16 @@
+//
+//  APObstaclePlane.h
+//  FighterPlanes
+//
+//  Created by Andrew Reardon on 10/4/14.
+//  Copyright 2014 Drenguin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface APObstaclePlane : APPlane {
+    
+}
+
+@end
---
+++ FighterPlanes/APObstaclePlane.m
@@ -0,0 +1,14 @@
+//
+//  APObstaclePlane.m
+//  FighterPlanes
+//
+//  Created by Andrew Reardon on 10/4/14.
+//  Copyright 2014 Drenguin. All rights reserved.
+//
+
+#import "APObstaclePlane.h"
+
+
+@implementation APObstaclePlane
+
+@end
