c6b57ae9b53f6e6062a733716854b0e08f158e5d
areardon11 <areardon11@hotmail.com>
2014-10-04 20:26:00 -0700

    worked on suicide plane

--- FighterPlanes/APEnemySuicidePlane.m
+++ FighterPlanes/APEnemySuicidePlane.m
@@ -16,14 +16,22 @@
     self = [super initWithImageNamed:@"suicidePlane.png"];
     if (!self) return(nil);
     
-    self.scale = .5f;
+    self.scale = .25f;
     self.health = 1;
     _speed = 2.0f;
     
     return self;
 }
 
+- (float) setDirection {
+    float deltx = self.heroSprite.position.x - self.position.x;
+    float delty = self.heroSprite.position.y - self.position.y;
+    
+    return 0.0;
+}
+
 - (void)update:(CCTime)delta {
+    self.rotation = [self setDirection];
     self.position = ccp(self.position.x+delta*_speed*sin(CC_DEGREES_TO_RADIANS(self.rotation)), self.position.y+delta*_speed*cos(CC_DEGREES_TO_RADIANS(self.rotation)));
 }
 
