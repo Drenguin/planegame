ff1d145cc6962b2e9932613b1ff32aaf6c7243e0
Patrick M <drenguino@gmail.com>
2014-11-23 15:39:36 -0800

    made stuff nicer and ready for release

--- FighterPlanes/Classes/IntroScene.m
+++ FighterPlanes/Classes/IntroScene.m
@@ -38,19 +38,23 @@
     //CCNodeColor *background = [CCNodeColor nodeWithColor:[CCColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.0f]];
     
     CCSprite *background = [CCSprite spriteWithImageNamed:@"startBackground.png"];
+    background.positionType = CCPositionTypeNormalized;
+    background.anchorPoint = ccp(0.5f, 0.5f);
+    background.position = ccp(0.51f, 0.5f);
+    background.scale = 0.7f;
     [self addChild:background];
     
     // Hello world
-    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Fighter Plane" fontName:@"Chalkduster" fontSize:38.0f];
+    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Shooter Plane" fontName:@"Chalkduster" fontSize:46.0f];
     label.positionType = CCPositionTypeNormalized;
     label.color = [CCColor redColor];
-    label.position = ccp(0.5f, 0.5f); // Middle of screen
+    label.position = ccp(0.5f, 0.8f); // Middle of screen
     [self addChild:label];
     
     // Helloworld scene button
-    CCButton *helloWorldButton = [CCButton buttonWithTitle:@"[ START ]" fontName:@"Courier New" fontSize:24.0f];
+    CCButton *helloWorldButton = [CCButton buttonWithTitle:@"[ START ]" fontName:@"Verdana-Bold" fontSize:24.0f];
     helloWorldButton.positionType = CCPositionTypeNormalized;
-    helloWorldButton.position = ccp(0.5f, 0.35f);
+    helloWorldButton.position = ccp(0.5f, 0.30f);
     [helloWorldButton setTarget:self selector:@selector(onSpinningClicked:)];
     [self addChild:helloWorldButton];
 
